7c22e6532d2271ffc2022809afe64945fe7390cb
leekijun <skynamoo@gmail.com>
2025-08-26 13:42:00 +0900

    2025-08-26 commit~

--- springboot/src/main/java/com/sch/springboot/controller/MemberController.java
+++ springboot/src/main/java/com/sch/springboot/controller/MemberController.java
@@ -6,18 +6,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
 
     @GetMapping("/member")
-    public String spring(@ModelAttribute Member member,  //form.html 이름과 Member 겟터 셋터 멤버변수 이름이 모두 같아야함
+    public String member(@ModelAttribute Member member,  //form.html 이름과 Member 겟터 셋터 멤버변수 이름이 모두 같아야함
                          Model model) { //파라메터를 받는 매개변수 자리
 
         model.addAttribute("member", member);
 
 
         return  "member"; //View name
+    }
+
+
+    @PostMapping("/member")
+    public String submit(@ModelAttribute Member member, Model model) {
+
+        System.out.println("Post!!");
+        model.addAttribute("member", member);
+        return "member";
     }
 }
--- springboot/src/main/java/com/sch/springboot/controller/SpringController.java
+++ springboot/src/main/java/com/sch/springboot/controller/SpringController.java
@@ -1,12 +1,24 @@
 package com.sch.springboot.controller;
 
+import com.sch.springboot.dto.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringController {
+
+    @ResponseBody  //View를 거치지 않고 문자혈 형태로 바로 전송
+    @GetMapping("/spring-api")
+    public String springApi(@ModelAttribute Member member) {
+
+        return member.toString();
+    }
+
+
     @GetMapping("/spring")
     public String spring(@RequestParam("name") String name,
                          @RequestParam("age") int age,
